c2267cb940e3eab39ce80586d029b14dfc0839b3
Sudhakaran T <sudhakarangt@gmail.com>
2024-11-21 23:48:28 +0530

    third

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Vishwaa! "
+    return "Hello Loossu! "
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='5000')
